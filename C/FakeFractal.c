b1357e0c203e1a6ce8179703ba5fe4ff77e0e3aa
Athos Andrade Silva de Araujo <100285947+athosaraujo@users.noreply.github.com>
2022-03-25 11:18:44 -0300

    Desafio de "tradução" do Professor Girotto

---
+++ C/FakeFractal.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <math.h>
+int lin;
+void baseMat(char matriz[]){			//Declara os valores de dentro do array matriz
+	for(int i=0;i<lin;i++){
+		for(int j=0;j<lin;j++){
+			matriz[i*lin+j]='-';
+		}
+	} matriz[(lin-1)*lin + (((lin+1)/2)-1)] = 'U';
+}
+
+void printMat(char matriz[]){		//Vai printar oq tem dentro do array
+	for(int i=0;i<lin;i++){
+		for(int j=0;j<lin;j++){
+			printf("%c", matriz[i*lin+j]);
+			} printf("\n");}}
+			
+void seed1(char matriz[], int inter)		// Adiciona caractéres dentro do array (as linhas da arvore)
+{
+    int i, j, k;
+
+    for(i = 0; i < lin; i++)
+    {
+        for(j = 0; j < lin; j++)
+        {
+            if(matriz[i*lin + j] == 'U')
+            {
+                for(k = 0; k < pow(2, inter - 1) ; k++)
+                {
+                    matriz[(i-k)*lin + j] = '1';
+                }
+                matriz[(i-k)*lin + j] = 'Y';
+            }
+        }
+    }
+}
+
+void seed2(char matriz[], int inter) // repete o seed1 com a outra metade
+{
+    int i, j, k;
+
+    for(i = 0; i < lin; i++)
+    {
+        for(j = 0; j < lin; j++)
+        {
+            if(matriz[i*lin + j] == 'Y')
+            {
+                for(k = 0; k < pow(2, inter - 1) ; k++)
+                {
+                    matriz[(i-k)*lin + j-k] = '1';
+                    matriz[(i-k)*lin + j+k] = '1';
+                }
+                matriz[(i-k)*lin + j-k] = 'U';
+                matriz[(i-k)*lin + j+k] = 'U';
+            }
+        }
+    }
+}
+
+void adNauseum(char matriz[], int inter){ 		//repete o codigo, criando um fractal de mentirinha
+	
+	if(inter>0){
+		seed1(matriz,inter);
+		seed2(matriz,inter);
+		--inter;	
+		adNauseum(matriz, inter);
+}}
+
+int main(){				// Executa as funções
+	int inter;
+	printf("Escolha o numero de inter: ");
+	scanf("%d", &inter);
+	
+	lin = pow(2, inter+1)+1;
+	
+	char* matriz[lin*lin];
+	
+	baseMat(matriz);
+	adNauseum(matriz,inter);
+	printMat(matriz);
+	 
+	 
+	getchar(); // Deixa a pessoa pressionar enter para encerrar o código
+	return 0;
+}
+
